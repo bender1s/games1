5c847a4ed1bac278715a6b368abe6c2949abceed
Kevin Bender <nycnpr@gmail.com>
2016-06-28 12:48:21 -0400

    formula work

--- game1/main.js
+++ game1/main.js
@@ -1,20 +1,25 @@
-function getRandomInt(max, min) {
-  return Math.floor(Math.random() * (max - min)) + min;
-};
 
-var randNum = getRandomInt(10, 1);
-console.log(randNum);
-
+// This creates random numbered array- plugs into formula -spits out result
 var arr = [];
 for (var i = 0, t = 10; i < 3; i++) {
   arr.push(Math.round(Math.random()* t))
-}
-arr = formula();
+  }
 function formula(a, b, c) {
-var answer =  a * b + c;
+ answer =  a * b + c;
 }
-
-
-console.log(formula);
-document.write(arr);
+formula(arr[0], arr[1], arr[2]);
 console.log(arr);
+console.log(answer);
+
+
+// player changer
+turnChange = true;
+console.log(turnChange);
+
+var userSolution = prompt("what is " + arr[0] + ' * ' + arr[1] + " + " + arr[2] );
+console.log(userSolution);
+while (userSolution != answer) {
+  var userSolution = prompt("what is " + arr[0] + ' * ' + arr[1] + " + " + arr[2] );
+
+}
+alert("Correct!")
