9d3cdc431cab9150d8c82181857f3d86c3b5706b
Kevin Bender <nycnpr@gmail.com>
2016-06-27 23:29:58 -0400

    file structure built

---
+++ game1/main.js
@@ -0,0 +1,20 @@
+function getRandomInt(max, min) {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+var randNum = getRandomInt(10, 1);
+console.log(randNum);
+
+var arr = [];
+for (var i = 0, t = 10; i < 3; i++) {
+  arr.push(Math.round(Math.random()* t))
+}
+arr = formula();
+function formula(a, b, c) {
+var answer =  a * b + c;
+}
+
+
+console.log(formula);
+document.write(arr);
+console.log(arr);
