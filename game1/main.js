406be30286bf606e98991c144a5756956423c125
Kevin Bender <nycnpr@gmail.com>
2016-06-29 13:10:41 -0400

    ahhhh!

--- game1/main.js
+++ game1/main.js
@@ -1,3 +1,5 @@
+var arr = [];
+var answer = [];
 document.addEventListener("DOMContentLoaded", function(event) {
     console.log("DOM fully loaded and parsed");
 //Math formula game
@@ -5,30 +7,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
 var playerOne = 10;
 var playerTwo = 10;
 var turn = true;
-var arr = [];
-var answer
+
+
 
 // This creates random numbered array- plugs into formula -spits out result
-function randomizer () {
-var arr = [];
-for (var i = 0, t = 10; i < 3; i++) {
-  arr.push(Math.round(Math.random()* t))
-    console.log(arr);
+function randomizer(min, max) {
+  for (var i = 0; i < 3; i++) {
+    arr.push(Math.floor(Math.random() * (max - min + 1)) + min);
+    }
   }
-};
-function algebra(a, b, c) {
-  var answer =  a * b + c;
-  answer.push()
-};
-algebra(arr[0], arr[1], arr[2]);
-  console.log(answer);
+function formula(a, b, c) {
+  answer.push(a * b + c);
 
-
+};
+randomizer(0, 10);
+formula(arr[0], arr[1], arr[2]);
+console.log(arr);
+console.log(answer);
 //Individual rounds
+
+
 function rounds() {
 if (turn == true) {
-   var playerChange = document.getElementById("player").innerHTML;
-   playerChange.innerHTML = "Player 1 Turn";
+  //  var playerChange = document.getElementById("player").innerHTML;
+  //  playerChange.innerHTML = "Player 1 Turn";
 var userSolution = `what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `;
 while (userSolution != answer) {
   var userSolution = `what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `;
@@ -39,21 +41,24 @@ console.log("playerone Score: " + playerOne);
    randomizer();
  turn = false;
 }
-};
-
-if (turn == false) {
-var userSolution = `what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `;
-while (userSolution != answer) {
-  var userSolution = `what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `;
-  playerTwo -= 1;
-} {
-("Correct!")
-console.log("playterTwo Score:" + playerTwo);
-  randomizer();
- turn = true;
+console.log(arr);
 }
-}};
-rounds();
+}
+
+
+// if (turn == false) {
+// var userSolution = `what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `;
+// while (userSolution != answer) {
+//   var userSolution = `what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `;
+//   playerTwo -= 1;
+// } {
+// ("Correct!")
+// console.log("playterTwo Score:" + playerTwo);
+//   randomizer();
+//  turn = true;
+// }
+//
+// rounds();
 
 if (playerOne && playerTwo >= 1) {
   rounds();
@@ -61,6 +66,6 @@ if (playerOne && playerTwo >= 1) {
   console.log("Player 2 wins!");
 } else {
   console.log("player 1 wins!");
-};
+}
+//}
 });
-// console.log("player points left: " + playerPoints);
