cfccc4355c42bd9c3241427c5713ff5b393b011e
Kevin Bender <nycnpr@gmail.com>
2016-06-30 21:55:30 -0400

    done

--- game1/main1.js
+++ game1/main1.js
@@ -1,4 +1,6 @@
 
+Mor junk code =(
+
 //Math formula game
 
 // var playerOne = 10;
@@ -20,43 +22,43 @@
 // };
 
 // function rounds() {
- if (turn == true) {
-var userSolution = prompt(`what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `);
-while (userSolution != answer)
-
-{
-   var userSolution = prompt(`what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `);
-   playerOne -= 1;
- } {
- ("Correct!")
- console.log("playerone Score: " + playerOne);
-    randomizer();
-  turn = false;
- }
- console.log(arr);
- }
-
-
-
- if (turn == false) {
- var userSolution = prompt(`what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `);
- while (userSolution != answer) {
-   var userSolution = prompt(`what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `);
-   playerTwo -= 1;
- } {
- ("Correct!")
- console.log("playterTwo Score:" + playerTwo);
-    randomizer();
-  turn = true;
- }
-rounds();
-
-if (playerOne && playerTwo >= 1) {
-  rounds();
-} else if (playerOne == 0) {
-  console.log("Player 2 wins!");
-} else {
-  console.log("player 1 wins!");
-}
+//  if (turn == true) {
+// var userSolution = prompt(`what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `);
+// while (userSolution != answer)
+//
+// {
+//    var userSolution = prompt(`what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `);
+//    playerOne -= 1;
+//  } {
+//  ("Correct!")
+//  console.log("playerone Score: " + playerOne);
+//     randomizer();
+//   turn = false;
+//  }
+//  console.log(arr);
+//  }
+//
+//
+//
+//  if (turn == false) {
+//  var userSolution = prompt(`what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `);
+//  while (userSolution != answer) {
+//    var userSolution = prompt(`what is: ${arr[0]} * ${arr[1]} + ${arr[2]} `);
+//    playerTwo -= 1;
+//  } {
+//  ("Correct!")
+//  console.log("playterTwo Score:" + playerTwo);
+//     randomizer();
+//   turn = true;
+//  }
+// rounds();
+//
+// if (playerOne && playerTwo >= 1) {
+//   rounds();
+// } else if (playerOne == 0) {
+//   console.log("Player 2 wins!");
+// } else {
+//   console.log("player 1 wins!");
+// }
 
 // console.log("player points left: " + playerPoints);
